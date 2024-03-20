9050aff70c33fa2e5fc29f025b8049cff8da3762
VictorDore <vdha20@student.aau.dk>
2024-03-20 10:31:02 +0100

    Update route skeleton extraction nad add sample main route

--- DTC/gridsystem.py
+++ DTC/gridsystem.py
@@ -73,11 +73,9 @@ class GridSystem:
         return (x_sum, y_sum)
     
     def extract_route_skeleton(self):
-        # Smooth MR
         smr = self.smooth_main_route()
-
-        # Remove outliers
         cmr = self.filter_outliers_in_main_route(smr)
+        rs = self.sample_main_route(cmr)
         
         # Sample MR with uniform distance interval
 
@@ -85,8 +83,8 @@ class GridSystem:
         smr = set()
         for (x1, y1) in self.main_route:
             ns = {(x2, y2) for (x2, y2) in self.main_route if self.calculate_euclidian_distance_between_cells((x1 + 0.5, y1 + 0.5), (x2 + 0.5, y2 + 0.5)) <= radius}
-            x_sum = sum(x for x, _ in ns)
-            y_sum = sum(y for _, y in ns)
+            x_sum = sum(x for x, _ in ns) + len(ns) * 0.5
+            y_sum = sum(y for _, y in ns) + len(ns) * 0.5
 
             if x_sum != 0:
                 x_sum /= len(ns)
@@ -97,14 +95,21 @@ class GridSystem:
         return smr
     
     def filter_outliers_in_main_route(self, smr: set, radius_prime: int = 20):
+        cmr = set()
         for (x1, y1) in smr:
             targets = {(x2, y2) for (x2, y2) in self.main_route if self.calculate_euclidian_distance_between_cells((x1, y1), (x2, y2)) <= radius_prime}
-            if targets < 0.01 * len(self.main_route):
-                smr.remove((x1, y1))
-        return smr
+            if len(targets) >= 0.01 * len(self.main_route):
+                cmr.add((x1, y1))
+        return cmr
     
-    def sample_main_route(self, distance_interval: int = 20):
-        pass
+    def sample_main_route(self, cmr: set, distance_interval: int = 20):
+        rs = set()
+        for c1 in cmr:
+            targets = {c2 for c2 in self.cmr if self.calculate_euclidian_distance_between_cells(c1, c2) <= distance_interval}
+            # targets should be greater than 1 to take self into account
+            if targets > 1:
+                rs.add(c1)
+        return rs
     
     @staticmethod
     def calculate_euclidian_distance_between_cells(cell1, cell2):
