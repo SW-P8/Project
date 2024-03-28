85b8efa5a5b88b46b831e853139199e66382f4ba
Sigurd <j.skadborg00@gmail.com>
2024-03-28 10:07:37 +0100

    more work on visualizer

--- DTC/visualizer.py
+++ DTC/visualizer.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 from DTC.gridsystem import GridSystem
 from DTC.trajectory import TrajectoryPointCloud, Trajectory, Point
+from typing import Tuple
 
 class Visualizer():
     def __init__(self, grid_system):
@@ -9,33 +10,59 @@ class Visualizer():
         self.fig, self.ax = plt.subplots()
 
     def visualize(self):
+        self.draw_main_route()
+        self.draw_populated_cells()
+        self.ax.legend(["point"])
+        plt.show()
+    
+    def draw_point_cloud(self):
         for trajectory in self.grid_system.pc.trajectories:
             for point in trajectory.points:
                 self.draw_point(point)
-                self.draw_safe_area(point, 3)
-        self.ax.legend(["point"])
-        plt.show()
+
+    def draw_main_route(self):
+        for point in self.grid_system.main_route:
+            self.draw_point(point)
 
     def draw_point(self, point):
-        x, y = point.get_coordinates()
-        self.ax.scatter(x, y, c='blue')
+        if isinstance(point, Point):
+            x, y = point.get_coordinates()
+        else:
+            x, y = point
+        self.ax.scatter(x, y, c='blue', s=1)
 
     def draw_safe_area(self, point, radius):
-        circle = plt.Circle(point.get_coordinates(), radius, color='blue', fill=False)
+        if isinstance(point, Point):
+            circle = plt.Circle(point.get_coordinates(), radius, color='blue', fill=False)
+        else:
+            circle = plt.Circle(point, radius, color='blue', fill=False)
         self.ax.add_patch(circle)
 
+    def draw_populated_cells(self):
+        for cell in self.grid_system.populated_cells:
+            self.draw_populated_cell(cell, self.grid_system.cell_size, self.grid_system.cell_size)
+
+    def draw_populated_cell(self, point, width, height):
+        if isinstance(point, Point):
+            rec = plt.Rectangle(point.get_coordinates(), width, height, color='green')
+        else:
+            rec = plt.Rectangle(point, width, height, color='green')
+        self.ax.add_patch(rec)
 
 if __name__ == "__main__":
-    print("hello world")
-
     t = Trajectory()
     time = datetime(2024, 1, 1, 1, 1, 1)
-    t.add_point(0,0,time)
-    t.add_point(15, 20, time)
-    t.add_point(30, 30, time)
-    t.add_point(37, 42, time)
+    t.add_point(116.51172, 39.92123,time)
+    t.add_point(116.51135, 39.93883, time)
+    t.add_point(116.51627, 39.91034, time)
+    t.add_point(116.47186, 39.91248, time)
+    t.add_point(116.47217, 39.92498, time)
+    t.add_point(116.47179, 39.90718, time)
+    t.add_point(116.45617, 39.90531, time)
     point_cloud = TrajectoryPointCloud()
     point_cloud.add_trajectory(t)
     grid_system = GridSystem(point_cloud)
-    v = Visualizer(grid_system)
-    v.visualize()
+    grid_system.create_grid_system()
+    grid_system.extract_main_route()
+    visualizer = Visualizer(grid_system)
+    visualizer.visualize()
