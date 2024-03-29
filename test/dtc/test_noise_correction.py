271ab5d61dad7dbe6bf8e0c891aebb895b8c704a
Lasse <lass5588@icloud.com>
2024-03-29 12:14:12 +0100

    removed test commneted out...

--- test/dtc/test_noise_correction.py
+++ test/dtc/test_noise_correction.py
@@ -22,25 +22,6 @@ class TestGridsystem():
         gs = gridsystem.GridSystem(pc)
         gs.create_grid_system()
         return gs
-
-    # def test_calculate_average_point(self):
-    #     pc = trajectory.TrajectoryPointCloud()
-    #     t = trajectory.Trajectory()
-
-    #     # Add point to use initialization point
-    #     t.add_point(2, 2, datetime(2024, 1, 1, 1, 1, 1))
-    #     # t.add_point(80, -80, datetime(2024, 1, 1, 1, 1, 2))
-    #     t.add_point(3, 3, datetime(2024, 1, 1, 1, 1, 3))
-
-    #     pc.add_trajectory(t)
-    #     gs = gridsystem.GridSystem(pc)
-    #     gs.create_grid_system()
-
-    #     # nc = noise_correction.NoiseCorrection(gs)
-    #     
-    #     # noise_correction.NoiseCorrection.calculate_average_point(t.points[1], t.points[0], t.points[2])
-    #     
-    #     assert t.points[0].longitude == 2
         
     def test_calculate_average_point(self):
         p1 = trajectory.Point(2, 2)
@@ -53,3 +34,4 @@ class TestGridsystem():
 
         assert expected_avg.latitude == p2.latitude
         assert expected_avg.longitude == p2.longitude
+
