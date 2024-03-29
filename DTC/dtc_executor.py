baa046f9d1c27d42cc586f4587077bc3002ab672
VictorDore <vdha20@student.aau.dk>
2024-03-29 08:58:26 +0100

    Benchmarking removed

--- DTC/dtc_executor.py
+++ DTC/dtc_executor.py
@@ -1,9 +1,8 @@
 from DTC.gridsystem import GridSystem
-from DTC.trajectory import Trajectory, Point, TrajectoryPointCloud
+from DTC.trajectory import Trajectory, TrajectoryPointCloud
 from database.taxi_data_handler import TaxiDataHandler
 from database.load_data import load_data_from_csv
 from database.db import init_db, new_tdrive_db_pool
-import time
 
 class DTCExecutor:
     def __init__(self, initialize_db: bool = False) -> None:
@@ -22,40 +21,18 @@ class DTCExecutor:
         trajectory = Trajectory()
         pc = TrajectoryPointCloud()
 
-        start_time = time.time()
         for _, timestamp, longitude, latitude, tid in records:
             if tid != tid_of_existing_trajectory:
                 pc.add_trajectory(trajectory)
                 trajectory = Trajectory()
 
             trajectory.add_point(longitude, latitude, timestamp)
-        end_time = time.time()
-        print("Trajectory point cloud creation completed in {:.5f} seconds".format(end_time - start_time))
-
 
         gs = GridSystem(pc)
-        start_time = time.time()
         gs.create_grid_system()
-        end_time = time.time()
-        print("Grid system creation completed in {:.5f} seconds".format(end_time - start_time))
-
-
-        start_time = time.time()
         gs.extract_main_route()
-        end_time = time.time()
-        print("Main route extraction completed in {:.5f} seconds".format(end_time - start_time))
-
-
-        start_time = time.time()
         gs.extract_route_skeleton()
-        end_time = time.time()
-        print("Route skeleton extraction completed in {:.5f} seconds".format(end_time - start_time))
-
-
-        start_time = time.time()
         gs.construct_safe_areas()
-        end_time = time.time()
-        print("Safe area construction completed in {:.5f} seconds".format(end_time - start_time))
 
         return gs
         
