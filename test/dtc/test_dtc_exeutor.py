d54cc033962c01381e84158a2f4e757b2adfc368
VictorDore <vdha20@student.aau.dk>
2024-03-29 12:25:51 +0100

    Fix brainfart

--- test/dtc/test_dtc_exeutor.py
+++ test/dtc/test_dtc_exeutor.py
@@ -9,15 +9,15 @@ class TestDTCExecutor:
         db.init_db()
         return DTCExecutor()
     
-    @pytest.mark.parametrize("input_value", range(1, 6))
+    @pytest.mark.parametrize("input_value", range(1, 8))
     def test_create_point_cloud_with_n_points_returns_correctly_with_single_trajectory(self, input_value, dtc_executor: DTCExecutor):
         pc = dtc_executor.create_point_cloud_with_n_points(input_value)
         assert len(pc.trajectories) == 1
-        assert len(pc.trajectories[0]) == input_value
+        assert len(pc.trajectories[0].points) == input_value
 
-    @pytest.mark.parametrize("input_value, second_trajectory_size", [(6, 1), (7, 2)])
+    @pytest.mark.parametrize("input_value, second_trajectory_size", [(8, 1), (9, 2)])
     def test_create_point_cloud_with_n_points_returns_correctly_with_two_trajectories(self, input_value, second_trajectory_size, dtc_executor: DTCExecutor):
         pc = dtc_executor.create_point_cloud_with_n_points(input_value)
         assert len(pc.trajectories) == 2
-        assert len(pc.trajectories[0]) == 5
-        assert len(pc.trajectories[1]) == second_trajectory_size    
+        assert len(pc.trajectories[0].points) == 5
+        assert len(pc.trajectories[1].points) == second_trajectory_size    
