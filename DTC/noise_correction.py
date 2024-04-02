d9d7785fd74ce89f75e24da319b0c221bb5c0785
Lasse <lass5588@icloud.com>
2024-04-02 10:45:56 +0200

    guard to not clean first or last

--- DTC/noise_correction.py
+++ DTC/noise_correction.py
@@ -5,14 +5,16 @@ class NoiseCorrection:
     def __init__(self, gridsystem: gridsystem.GridSystem):
         self.gridsystem = gridsystem
 
+    # TODO decide how to handle if p-1 or p+1 is also noise, such that we do not correct noise with noise.
     def noise_detection(self, trajectory):
         for i, point in enumerate(trajectory.points):
             nearest_anchor, dist = self.gridsystem.find_nearest_neighbor_from_candidates(point, self.gridsystem.route_skeleton)
             
             if dist >= self.gridsystem.safe_areas[nearest_anchor]:
-                self.correct_noisy_point(trajectory, i)
+                # Ensures that we do not try to clean first or last element. Should be improved!
+                if i != 0 and i != len(trajectory.points) - 1:
+                    self.correct_noisy_point(trajectory, i)
 
-    # TODO decide how to handle if p-1 or p+1 is also noise, such that we do not correct noise with noise.
     def correct_noisy_point(self, trajectory: trajectory.Trajectory, point_id: int):
         self.calculate_average_point(trajectory.points[point_id], trajectory.points[point_id - 1], trajectory.points[point_id + 1])
         
