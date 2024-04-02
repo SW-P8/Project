0334434acccf09696cc8779c3c508462cd2e6c16
Sigurd <j.skadborg00@gmail.com>
2024-04-02 10:28:41 +0200

    format

--- DTC/visualizer.py
+++ DTC/visualizer.py
@@ -4,6 +4,7 @@ from DTC.gridsystem import GridSystem
 from DTC.trajectory import TrajectoryPointCloud, Trajectory, Point
 from DTC.dtc_executor import DTCExecutor
 
+
 class Visualizer():
     def __init__(self, grid_system):
         self.grid_system = grid_system
@@ -28,7 +29,7 @@ class Visualizer():
         self.draw_safe_area()
         plt.tight_layout()
         plt.show()
-    
+
     def draw_point_cloud(self):
         for trajectory in self.grid_system.pc.trajectories:
             for point in trajectory.points:
@@ -42,7 +43,7 @@ class Visualizer():
         print(self.grid_system.route_skeleton)
         for point in self.grid_system.route_skeleton:
             self.draw_point(self.sk_ax, point)
-    
+
     def draw_safe_area(self):
         for anchor, radius in self.grid_system.safe_areas.items():
             self.draw_circle(self.sk_ax, anchor, radius)
@@ -56,21 +57,25 @@ class Visualizer():
 
     def draw_circle(self, ax, point, radius):
         if isinstance(point, Point):
-            circle = plt.Circle(point.get_coordinates(), radius, color='blue', fill=False)
+            circle = plt.Circle(point.get_coordinates(),
+                                radius, color='blue', fill=False)
         else:
             circle = plt.Circle(point, radius, color='blue', fill=False)
         ax.add_patch(circle)
 
     def draw_populated_cells(self):
         for cell in self.grid_system.populated_cells:
-            self.draw_populated_cell(self.mr_ax, cell, self.grid_system.cell_size, self.grid_system.cell_size)
+            self.draw_populated_cell(
+                self.mr_ax, cell, self.grid_system.cell_size, self.grid_system.cell_size)
 
     def draw_populated_cell(self, ax, point, width, height):
         if isinstance(point, Point):
-            rec = plt.Rectangle(point.get_coordinates(), width, height, color='green')
+            rec = plt.Rectangle(point.get_coordinates(),
+                                width, height, color='green')
         else:
             rec = plt.Rectangle(point, width, height, color='green')
         ax.add_patch(rec)
+
 
 if __name__ == "__main__":
     dtc_executor = DTCExecutor()
