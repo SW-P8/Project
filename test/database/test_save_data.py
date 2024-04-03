6bcc52c9fa513790956adae2b1eb4f2dd744eba4
Martin Mortensen <martinmortensen@pop-os.localdomain>
2024-04-03 11:55:55 +0200

    added to test fixture

--- test/database/test_save_data.py
+++ test/database/test_save_data.py
@@ -6,7 +6,7 @@ from DTC import gridsystem, trajectory
 from database import save_data
 from database.db import init_db
 
-
+@pytest.fixture
 def test_save_data_with_fivepoint_grid_constructed_save_area():
     pc = trajectory.TrajectoryPointCloud()
     t = trajectory.Trajectory()
@@ -30,8 +30,40 @@ def test_save_data_with_fivepoint_grid_constructed_save_area():
     print(gs.pc.get_shifted_min())
     try:
         save_data.save_data(gs,db)
+        safe_areas = get_latest_min_coords_and_datapoints(db)
+        expected_coords = []
+        actual_coords = []
+        print(safe_areas)
+        print(gs.safe_areas)
+        for (anc_x, anc_y, rad) in safe_areas:
+            expected_coords.append((anc_x, anc_y, rad))
+        for (x, y), radi in gs.safe_areas.items():
+            actual_coords.append((x, y, radi))
+        return(actual_coords, expected_coords)
+            
     except Exception as e:
         print("test_failed", e)
+        return None
 
-    
-test_save_data_with_fivepoint_grid_constructed_save_area()
+
+def get_latest_min_coords_and_datapoints(db):
+    conn = db.getconn()
+    cursor = conn.cursor()
+    safe_areas = []
+    try:
+        cursor.execute("SELECT model_id FROM DTC_model_min_coords ORDER BY model_id DESC LIMIT 1")
+        result = cursor.fetchone()[0]
+        print(result)
+        if result:
+            cursor.execute("SELECT anchor_x, anchor_y, radius FROM DTC_model_safe_areas WHERE model_id = %s", (result,))
+            safe_areas = cursor.fetchall()
+            return safe_areas
+    except Exception as error:
+        print("Error fetching latest min_coords:", error)
+    finally:
+        cursor.close()
+    return None
+
+def test_save_data(test_save_data_with_fivepoint_grid_constructed_save_area):
+    result = test_save_data_with_fivepoint_grid_constructed_save_area
+    assert result[0] == result[1]
