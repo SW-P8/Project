6f5a81141407a1487dc76fcae27b099fcdba2fde
VictorDore <vdha20@student.aau.dk>
2024-04-17 17:17:37 +0200

    Methods rewritten to use smaller subparts of previous steps

--- DTC/route_skeleton.py
+++ DTC/route_skeleton.py
@@ -14,14 +14,17 @@ class RouteSkeleton:
     @staticmethod
     def smooth_main_route(main_route: set, radius: int) -> defaultdict[set]:
         process_count = mp.cpu_count()
-        splits =  CollectionUtils.split(main_route, process_count)
+        sorted_main_route = CollectionUtils.sort_collection_of_tuples(main_route)
+        sub_main_routes =  CollectionUtils.split(sorted_main_route, process_count)
         tasks = []
         pipe_list = []
 
-        for split in splits:
-            if split != []:
+        for sub_main_route in sub_main_routes:
+            if sub_main_route != []:
                 recv_end, send_end = mp.Pipe(False)
-                task = mp.Process(target=RouteSkeleton.smooth_sub_main_route, args=(split, main_route, radius, send_end))
+                bounds = CollectionUtils.get_min_max_with_padding_from_collection_of_tuples(sub_main_route, radius)
+                sub_main_route_with_padding = CollectionUtils.get_tuples_within_bounds(sorted_main_route, bounds)
+                task = mp.Process(target=RouteSkeleton.smooth_sub_main_route, args=(sub_main_route, sub_main_route_with_padding, radius, send_end))
                 tasks.append(task)
                 pipe_list.append(recv_end)
                 task.start()
@@ -37,7 +40,7 @@ class RouteSkeleton:
         return smoothed_main_route
     
     @staticmethod
-    def smooth_sub_main_route(sub_main_route, main_route, radius, send_end):
+    def smooth_sub_main_route(sub_main_route, sub_main_route_with_padding, radius, send_end):
         sub_smoothed_main_route = defaultdict(set)
         for (x1, y1) in sub_main_route:
             x_sum = 0
@@ -45,7 +48,7 @@ class RouteSkeleton:
             count = 0
             for i in range(x1 - radius, x1 + radius + 1):
                 for j in range(y1 - radius, y1 + radius + 1):
-                    if (i,j) in main_route and DistanceCalculator.calculate_euclidian_distance_between_cells((x1, y1), (i, j)) <= radius:
+                    if (i,j) in sub_main_route_with_padding and DistanceCalculator.calculate_euclidian_distance_between_cells((x1, y1), (i, j)) <= radius:
                         x_sum += i + 0.5
                         y_sum += j + 0.5
                         count += 1
@@ -66,14 +69,18 @@ class RouteSkeleton:
         connection_threshold_factor = 0.01
         connection_threshold = connection_threshold_factor * main_route_length
         process_count = mp.cpu_count()
-        splits =  CollectionUtils.split(smoothed_main_route.values(), process_count)
+        sorted_smoothed_main_route_keys = CollectionUtils.sort_collection_of_tuples(smoothed_main_route.keys())
+        sub_smoothed_main_route_keys_list =  CollectionUtils.split(sorted_smoothed_main_route_keys, process_count)
         tasks = []
         pipe_list = []
 
-        for split in splits:
-            if split != []:
+        for sub_smoothed_main_route_keys in sub_smoothed_main_route_keys_list:
+            if sub_smoothed_main_route_keys != []:
                 recv_end, send_end = mp.Pipe(False)
-                task = mp.Process(target=RouteSkeleton.filter_outliers_in_sub_smoothed_main_route, args=(split, smoothed_main_route, connection_threshold, radius_prime, send_end))
+                bounds = CollectionUtils.get_min_max_with_padding_from_collection_of_tuples(sub_smoothed_main_route_keys, radius_prime)
+                sub_smoothed_main_route_with_padding_keys = CollectionUtils.get_tuples_within_bounds(sorted_smoothed_main_route_keys, bounds)
+                sub_smoothed_main_route_with_padding_dict = CollectionUtils.get_sub_dict_from_subset_of_keys(smoothed_main_route, sub_smoothed_main_route_with_padding_keys)
+                task = mp.Process(target=RouteSkeleton.filter_outliers_in_sub_smoothed_main_route, args=(sub_smoothed_main_route_keys, sub_smoothed_main_route_with_padding_dict, connection_threshold, radius_prime, send_end))
                 tasks.append(task)
                 pipe_list.append(recv_end)
                 task.start()
@@ -89,14 +96,14 @@ class RouteSkeleton:
         return contracted_main_route
         
     @staticmethod
-    def filter_outliers_in_sub_smoothed_main_route(sub_smoothed_main_route: list, smoothed_main_route: dict, connection_threshold, radius_prime, send_end):
+    def filter_outliers_in_sub_smoothed_main_route(sub_smoothed_main_route_keys: list, sub_smoothed_main_route_with_padding: dict, connection_threshold, radius_prime, send_end):
         sub_contracted_main_route = defaultdict(set)
-        for cells in sub_smoothed_main_route:
-            for (x1, y1) in cells:
+        for key in sub_smoothed_main_route_keys:
+            for (x1, y1) in sub_smoothed_main_route_with_padding[key]:
                 targets = 0
                 for i in range(int(x1) - radius_prime, int(x1) + radius_prime + 1):
                     for j in range(int(y1) - radius_prime, int(y1) + radius_prime + 1):
-                        candidates = smoothed_main_route.get((i, j))
+                        candidates = sub_smoothed_main_route_with_padding.get((i, j))
                         if candidates is not None:
                             for (x2, y2) in candidates:
                                 if DistanceCalculator.calculate_euclidian_distance_between_cells((x1, y1), (x2, y2)) <= radius_prime:
@@ -108,14 +115,18 @@ class RouteSkeleton:
     @staticmethod
     def sample_contracted_main_route(contracted_main_route: dict, distance_interval: int) -> set:
         process_count = mp.cpu_count()
-        splits =  CollectionUtils.split(contracted_main_route.values(), process_count)
+        sorted_contracted_main_route_keys = CollectionUtils.sort_collection_of_tuples(contracted_main_route.keys())
+        sub_contracted_main_route_keys_list =  CollectionUtils.split(sorted_contracted_main_route_keys, process_count)
         tasks = []
         pipe_list = []
 
-        for split in splits:
-            if split != []:
+        for sub_contracted_main_route_keys in sub_contracted_main_route_keys_list:
+            if sub_contracted_main_route_keys != []:
                 recv_end, send_end = mp.Pipe(False)
-                task = mp.Process(target=RouteSkeleton.sample_sub_contracted_main_route, args=(split, contracted_main_route, distance_interval, send_end))
+                bounds = CollectionUtils.get_min_max_with_padding_from_collection_of_tuples(sub_contracted_main_route_keys, distance_interval)
+                sub_contracted_main_route_with_padding_keys = CollectionUtils.get_tuples_within_bounds(sorted_contracted_main_route_keys, bounds)
+                sub_contracted_main_route_with_padding_dict = CollectionUtils.get_sub_dict_from_subset_of_keys(contracted_main_route, sub_contracted_main_route_with_padding_keys)
+                task = mp.Process(target=RouteSkeleton.sample_sub_contracted_main_route, args=(sub_contracted_main_route_keys, sub_contracted_main_route_with_padding_dict, distance_interval, send_end))
                 tasks.append(task)
                 pipe_list.append(recv_end)
                 task.start()
@@ -130,14 +141,14 @@ class RouteSkeleton:
         return route_skeleton
         
     @staticmethod
-    def sample_sub_contracted_main_route(sub_contracted_main_route, contracted_main_route, distance_interval, send_end):
+    def sample_sub_contracted_main_route(sub_contracted_main_route_keys, sub_contracted_main_route_with_padding, distance_interval, send_end):
         sub_route_skeleton = set()
-        for cells in sub_contracted_main_route:
-            for (x1, y1) in cells:
+        for key in sub_contracted_main_route_keys:
+            for (x1, y1) in sub_contracted_main_route_with_padding[key]:
                 targets = 0
                 for i in range(int(x1) - distance_interval, int(x1) + distance_interval + 1):
                     for j in range(int(y1) - distance_interval, int(y1) + distance_interval + 1):
-                        candidates = contracted_main_route.get((i, j))
+                        candidates = sub_contracted_main_route_with_padding.get((i, j))
                         if candidates is not None:
                             for (x2, y2) in candidates:
                                 if DistanceCalculator.calculate_euclidian_distance_between_cells((x1, y1), (x2, y2)) <= distance_interval:
