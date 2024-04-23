944212eff0d3dedef31f8ba5d66a455eff904e72
Lasse <lass5588@icloud.com>
2024-04-23 22:27:04 +0200

    tests for updated_confidence and timed_decay

--- test/dtc/test_construct_safe_area.py
+++ test/dtc/test_construct_safe_area.py
@@ -2,6 +2,7 @@ import pytest
 from datetime import datetime, timedelta
 from DTC.construct_safe_area import ConstructSafeArea, SafeArea
 from DTC.distance_calculator import DistanceCalculator
+from DTC.point import Point
 from DTC.trajectory import Trajectory, TrajectoryPointCloud
 from DTC.gridsystem import GridSystem
 
@@ -118,7 +119,7 @@ class TestSafeArea():
     @pytest.fixture
     def default_safe_area(self):
         cover_set = {(5, 5)}
-        return SafeArea(cover_set, (5, 5), 0.1)
+        return SafeArea(cover_set, (5, 5), decrease_factor=0.1, confidence_change=0.01, cardinality_normalisation=100000, cardinality_squish=0.1, max_confidence_change=0.1)
 
     @pytest.mark.parametrize("normalised_cardinality, expected_result",
         [
@@ -182,15 +183,63 @@ class TestSafeArea():
         assert default_safe_area.confidence == 0.2
         assert default_safe_area.timestamp == new_time
 
+    # Test that confidence should be impacted over time.
     def test_get_confidence_and_time_change_over_time(self, default_safe_area):
         new_time = datetime.now() + timedelta(hours=0)
         
-        # not changed if same time 
+        # Should not change, as time is unchanged.
         assert (1, new_time) == default_safe_area.get_current_confidence_with_timestamp(new_time)
 
         new_time = datetime.now() + timedelta(hours=100)
         
         assert (1, new_time) != default_safe_area.get_current_confidence_with_timestamp(new_time)
 
+    def test_update_confidence_outside_radius(self, default_safe_area):
+        test_point = Point(10, 10, datetime.now())
+        default_safe_area.cardinality = 10
+
+        start_cardinality = default_safe_area.cardinality
+        start_confidence = default_safe_area.confidence
         
+        default_safe_area.update_confidence(10, test_point)
+
+        # Cardinality should not change as it is not within radius of safe area.
+        assert start_cardinality == default_safe_area.cardinality 
+        assert start_confidence > default_safe_area.confidence
+
+    def test_update_confidence_inside_radius(self, default_safe_area):
+        test_point = Point(5.5, 5.5, datetime.now())
+        default_safe_area.confidence = 0.5
+        default_safe_area.cardinality = 10
+
+        start_cardinality = default_safe_area.cardinality
+        start_confidence = default_safe_area.confidence
+
+        default_safe_area.update_confidence(0.5, test_point)
+        
+        # Cardinality should change as it is within radius of safe area.
+        assert start_cardinality < default_safe_area.cardinality
+        assert start_cardinality + 1 == default_safe_area.cardinality
+        assert start_confidence < default_safe_area.confidence
+
+    def test_calculate_timed_decay_do_not_change(self, default_safe_area):
+        default_safe_area.cardinality = 10
+        
+        assert 0 == default_safe_area.calculate_timed_decay(0.1)
+
+
+    def test_calculate_timed_decay_change(self, default_safe_area):
+        default_safe_area.cardinality = 10
+        default_safe_area.decay_factor = 0.1
+
+        assert 0 < default_safe_area.calculate_timed_decay(1)
+
+    def test_calculate_timed_decay_change_with_delta(self, default_safe_area):
+        default_safe_area.cardinality = 10
+        default_safe_area.decay_factor = 0.1
+        
+        decay_low = default_safe_area.calculate_timed_decay(0.1)
+        decay_high = default_safe_area.calculate_timed_decay(1)
+        
+        assert decay_low < decay_high
 
