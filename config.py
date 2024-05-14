6f670703399c21e4e5924b9ccd7bfb2fa9615616
Sigurd <j.skadborg00@gmail.com>
2024-05-14 17:48:56 +0200

    added config file

---
+++ config.py
@@ -0,0 +1,24 @@
+# Direction constants
+NORTH = 0
+EAST = 90
+SOUTH = 180
+WEST = 270
+
+# Metric constants
+CELL_SIZE = 5         # Based on observation in DTC paper that minimal width of a road is 5m
+NEIGHBORHOOD_SIZE = 9 # To be determined but DTC uses 9
+
+
+# Safe Area
+decrease_factor = 0.01
+decay_factor = 1 / (60*60*24)
+confidence_change = 0.01
+cardinality_squish = 0.01
+max_confidence_change = 0.1
+
+
+# Route Skeleton
+smooth_radius = 25
+filtering_list_radius = 20
+distance_interval = 20
+distance_scale = 0.2
