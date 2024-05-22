cc1adef887d8442602537f6508940f21571b2162
VictorDore <vdha20@student.aau.dk>
2024-05-22 15:21:23 +0200

    Throughput for updating safe areas added

--- test/benchmark/test_throughput_benchmark.py
+++ test/benchmark/test_throughput_benchmark.py
@@ -3,15 +3,21 @@ from DTC.noise_correction import NoiseCorrection
 from DTC.trajectory import Trajectory, TrajectoryPointCloud
 from DTC.gridsystem import GridSystem
 from DTC.distance_calculator import DistanceCalculator
+from DTC.route_skeleton import RouteSkeleton
+from onlinedtc.increment import update_safe_area
 from copy import deepcopy
 import config
 
 class TestThroughputBenchmark:
-    @pytest.mark.bm_throughput
-    def test_cleaning_throughput(self, benchmark):
+    @pytest.fixture
+    def trajectory(self):
         trajectory = Trajectory()
         for i in range(1000):
             trajectory.add_point(i * 0.00001, 0.0001, None)
+        return trajectory
+    
+    @pytest.fixture
+    def grid_system(self, trajectory):
         point_cloud = TrajectoryPointCloud()
         point_cloud.add_trajectory(trajectory)
         grid_system = GridSystem(point_cloud)
@@ -19,6 +25,10 @@ class TestThroughputBenchmark:
         grid_system.extract_main_route()
         grid_system.extract_route_skeleton()
         grid_system.construct_safe_areas(0)
+        return grid_system
+
+    @pytest.mark.bm_throughput
+    def test_cleaning_throughput(self, benchmark, trajectory, grid_system):
         noise_corrector = NoiseCorrection(grid_system.safe_areas, grid_system.initialization_point, with_iteration=False)
 
         # Make every second point noisy
@@ -47,6 +57,32 @@ class TestThroughputBenchmark:
         benchmark.pedantic(noise_detection_wrapper, rounds=rounds, iterations=iterations, warmup_rounds=warmup_rounds)
 
     @pytest.mark.bm_throughput
-    def test_iterating_throughput(self, benchmark):
-        trajectory = None
-        print("iterating yup")
+    def test_iterating_throughput(self, benchmark, trajectory,grid_system):
+        old_smoothed_main_route = RouteSkeleton.smooth_main_route(grid_system.main_route)
+        noise_corrector = NoiseCorrection(grid_system.safe_areas, grid_system.initialization_point)
+
+        # Make every second point noisy
+        for i in range(1000):
+            trajectory.points[i].latitude = DistanceCalculator.shift_point_with_bearing(trajectory.points[i], 105, config.SOUTH)[1]
+
+        noise_corrector.noise_detection(trajectory)
+
+        # Calculate the total number of deepcopies needed
+        rounds = 5
+        iterations = 3
+        warmup_rounds = 2
+        total_iterations = (rounds + warmup_rounds) * iterations + 1
+
+        # Pre-create deepcopies of trajectory
+        list_of_lists_of_safe_areas_and_smr = [[(deepcopy(grid_system.safe_areas), deepcopy(old_smoothed_main_route)) for _ in range(10)] for _ in range(total_iterations)]
+    
+        # Index to track the current deepcopy to use
+        index = [0]
+
+        def update_safe_area_wrapper():
+            current_list_of_safe_areas_and_smr = list_of_lists_of_safe_areas_and_smr[index[0]]
+            for current_safe_areas, current_smr in current_list_of_safe_areas_and_smr:
+                update_safe_area(current_safe_areas, grid_system.initialization_point, current_smr)
+            index[0] += 1
+
+        benchmark.pedantic(update_safe_area_wrapper, rounds=rounds, iterations=iterations, warmup_rounds=warmup_rounds)
